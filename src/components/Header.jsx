9a7d8b9928943db208713f21140d949341a8ea75
dducharm <dakotah.b.ducharme@aexp.com>
2023-04-28 11:55:28 -0700

    setting up basic components

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header(){
+    return (
+        <div>
+            <header>
+                <h1>Store Front</h1>
+                <ul>
+                    <li><a href="/">home</a></li>
+                    <li><a path="/shop">nav item</a></li>
+                    <li><a path="/cart">nav item</a></li>
+                </ul>
+            </header>
+        </div>
+    )
+}
