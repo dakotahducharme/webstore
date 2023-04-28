7af5b7bc71e51cdb61d32c29a211864d0741207b
dducharm <dakotah.b.ducharme@aexp.com>
2023-04-28 12:35:44 -0700

    pushing to GH

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ export default function Header(){
                     <li><a href="/">home</a></li>
                     <li><a path="/shop">nav item</a></li>
                     <li><a path="/cart">nav item</a></li>
+                    <li>4</li>
                 </ul>
             </header>
         </div>
